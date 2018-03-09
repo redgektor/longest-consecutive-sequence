551774351076c66afb827b8a5afc62edb3c868ca
redgektor <redgektor11@gmail.com>
2018-03-10 00:03:40 +0300

    What about O(n)?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+	var result = 0;
+  for (var i = 0; i < array.length; i++) {
+  	var count = 0; 
+	findSecond(array[i]);
+  	if ( count > result )
+  		result = count+1;
+  }
+  return result;
+
+	function findSecond(element) {
+		if (array.indexOf(element+1) > 0) {
+			count++;
+			findSecond(element+1);
+		}
+	}
 }
